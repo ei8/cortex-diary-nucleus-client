9cb7665d7a65b3cc706c5703eea90bf059c47303
Junvic <contact@junvic.me>
2022-08-27 09:26:58 +0800

    refactor: make subscription client generic

--- src/main/In/HttpSubscriptionClient.cs
+++ src/main/In/HttpSubscriptionClient.cs
@@ -1,4 +1,6 @@
 ﻿using ei8.Cortex.Subscriptions.Common;
+using ei8.Cortex.Subscriptions.Common.Extensions;
+using ei8.Cortex.Subscriptions.Common.Receivers;
 using neurUL.Common.Http;
 using NLog;
 using Polly;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace ei8.Cortex.Diary.Nucleus.Client.In
 {
-    public class HttpSubscriptionClient : ISubscriptionClient
+    public class HttpSubscriptionClient<T> : ISubscriptionClient<T> where T : IReceiverInfo
     {
         private readonly IRequestProvider requestProvider;
 
@@ -18,22 +20,24 @@ namespace ei8.Cortex.Diary.Nucleus.Client.In
             .WaitAndRetryAsync(
                 3,
                 attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)),
-                (ex, _) => HttpSubscriptionClient.logger.Error(ex, "Error occurred while communicating with Neurul Cortex. " + ex.InnerException?.Message)
+                (ex, _) => HttpSubscriptionClient<T>.logger.Error(ex, "Error occurred while communicating with Neurul Cortex. " + ex.InnerException?.Message)
             );
 
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
-        private static readonly string subscriptionsPath = "nuclei/d23/subscriptions";
+        private static readonly string subscriptionsPath = "nuclei/d23/subscriptions/receivers";
 
         public HttpSubscriptionClient(IRequestProvider requestProvider = null)
         {
             this.requestProvider = requestProvider ?? Locator.Current.GetService<IRequestProvider>();
         }
 
-        public async Task AddSubscriptionAsync(string baseUrl, BrowserSubscriptionInfo subscriptionInfo, string bearerToken, CancellationToken cancellationToken = default)
+        public async Task AddSubscriptionAsync(string baseUrl, IAddSubscriptionReceiverRequest<T> request, string bearerToken, CancellationToken cancellationToken = default)
         {
-            await HttpSubscriptionClient.exponentialRetryPolicy.ExecuteAsync(async () =>
+            var requestUrl = $"{baseUrl}{HttpSubscriptionClient<T>.subscriptionsPath}/{request.ReceiverInfo.GetSubscriptionPath()}";
+
+            await HttpSubscriptionClient<T>.exponentialRetryPolicy.ExecuteAsync(async () =>
             {
-                await this.requestProvider.PostAsync($"{baseUrl}{HttpSubscriptionClient.subscriptionsPath}", subscriptionInfo, token: bearerToken);
+                await this.requestProvider.PostAsync(requestUrl, request, token: bearerToken);
             });
         }
     }
--- src/main/In/ISubscriptionClient.cs
+++ src/main/In/ISubscriptionClient.cs
@@ -1,11 +1,12 @@
 ﻿using ei8.Cortex.Subscriptions.Common;
+using ei8.Cortex.Subscriptions.Common.Receivers;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ei8.Cortex.Diary.Nucleus.Client.In
 {
-    public interface ISubscriptionClient
+    public interface ISubscriptionClient<T> where T : IReceiverInfo
     {
-        Task AddSubscriptionAsync(string baseUrl, BrowserSubscriptionInfo subscriptionInfo, string bearerToken, CancellationToken cancellationToken = default);
+        Task AddSubscriptionAsync(string baseUrl, IAddSubscriptionReceiverRequest<T> receiverInfo, string bearerToken, CancellationToken cancellationToken = default);
     }
 }
