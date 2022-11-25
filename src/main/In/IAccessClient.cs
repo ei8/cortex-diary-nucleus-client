474ff03ea2d832882d68fce8dc35c00ebe8b3cdb
Junvic <contact@junvic.me>
2022-11-25 16:52:30 +0800

    Support for access request handler (#9)
    * feature: add create access request method
    
    * refactor: moved access request to separate client
    
    * Update Client.csproj
    
    Co-authored-by: elmerbool <35117496+elmerbool@users.noreply.github.com>

---
+++ src/main/In/IAccessClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Diary.Nucleus.Client.In
+{
+    public interface IAccessClient
+    {
+        Task CreateNeuronAccessRequest(string avatarUrl, string neuronId, int expectedVersion, string bearerToken, CancellationToken token = default(CancellationToken));
+    }
+}
